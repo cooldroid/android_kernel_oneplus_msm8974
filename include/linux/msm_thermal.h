618d7b154b17d12f4ac4d573a1cfdd5eea03015e
Zachariah <zkennedy87@gmail.com>
2015-03-10 01:59:02 -0400

    Thermal: Cleanup for Franco's simplified Thermal Control
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	include/linux/msm_thermal.h

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -40,6 +40,7 @@ struct msm_thermal_data {
 };
 
 #ifdef CONFIG_THERMAL_MONITOR
+<<<<<<< HEAD
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 #else
@@ -48,6 +49,13 @@ static inline int msm_thermal_init(struct msm_thermal_data *pdata)
 	return -ENOSYS;
 }
 static inline int msm_thermal_device_init(void)
+=======
+extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
+	bool is_max);
+#else
+static inline int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
+	bool is_max)
+>>>>>>> aed0969... Thermal: Cleanup for Franco's simplified Thermal Control
 {
 	return -ENOSYS;
 }
