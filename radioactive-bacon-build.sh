b37a125bb9e92083a74a537aabcf5517c2cf1ee4
acuicultor <ramoncuenca4@gmail.com>
2016-01-15 09:09:02 +0100

    Update build version

--- radioactive-bacon-build.sh
+++ radioactive-bacon-build.sh
@@ -16,7 +16,7 @@ DEFCONFIG="radioactive_defconfig"
 
 # Kernel Details
 BASE_RADIOACTIVE_VER="RADIOACTIVE"
-VER="_CAF_Marshmallow_V1.11"
+VER="_CAF_Marshmallow_V1.12"
 RADIOACTIVE_VER="$BASE_RADIOACTIVE_VER$VER"
 
 # Vars
