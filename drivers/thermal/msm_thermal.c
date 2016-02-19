1523f91a7e466c6e179190d594c1e73e18f42ae2
nikhil18 <nikhilgolechha1@gmail.com>
2016-02-19 17:55:38 +0530

    lower temp threshold to 50

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -26,7 +26,7 @@
 #include <linux/of.h>
 #include <linux/hrtimer.h>
 
-unsigned int temp_threshold = 65;
+unsigned int temp_threshold = 50;
 module_param(temp_threshold, int, 0755);
 
 static struct thermal_info {
--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -40,22 +40,11 @@ struct msm_thermal_data {
 };
 
 #ifdef CONFIG_THERMAL_MONITOR
-<<<<<<< HEAD
-extern int msm_thermal_init(struct msm_thermal_data *pdata);
-extern int msm_thermal_device_init(void);
-#else
-static inline int msm_thermal_init(struct msm_thermal_data *pdata)
-{
-	return -ENOSYS;
-}
-static inline int msm_thermal_device_init(void)
-=======
 extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
 	bool is_max);
 #else
 static inline int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
 	bool is_max)
->>>>>>> aed0969... Thermal: Cleanup for Franco's simplified Thermal Control
 {
 	return -ENOSYS;
 }
