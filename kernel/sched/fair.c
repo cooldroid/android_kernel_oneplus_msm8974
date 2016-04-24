425e5e22305ebc784607db865e1b9914d658bd47
DerRomtester <dieb.stefan.96@gmail.com>
2016-04-24 13:41:22 +0200

    cpuquiet: introduce

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2640,7 +2640,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		update_rq_runnable_avg(rq, rq->nr_running);
-		inc_nr_running(rq);
+		__inc_nr_running(rq);
 	}
 	hrtick_update(rq);
 }
